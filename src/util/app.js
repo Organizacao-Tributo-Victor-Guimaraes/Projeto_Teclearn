e5fe31aeee01b47f6e234dfbd940faa702e0af8a
JoaoGuatura <joaoguatura@hotmail.com>
2024-09-04 00:40:35 -0300

    implementando PWA
    No momento deste commit, o PWA está funcionando corretamente. Foi adicionado as imagens de captura de tela e ícone as configurações do manifest. Assim como foi criado um manifest.json e um service-worker, de modo que no navegador em Application, esteja aparecendo que o service-worker está funcionando corretamente, assim como no teste de simulação de ficar sem internet, ele também funciona.

--- src/util/app.js
+++ src/util/app.js
@@ -1,3 +1,19 @@
+//Registrando o service worker
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch(error => {
+        console.log('Falha ao registrar o Service Worker:', error);
+      });
+  });
+}
+
+//Sistema de quiz
+
 const contadorVida = document.getElementById('contador-vida');
 const valorInicial = 5;
 const pergunta = document.getElementById('pergunta');
